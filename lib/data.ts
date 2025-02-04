ba0cd087f6346b5c157ec1e2150fa750f3fc9c13
Aditya <adityaanandx@gmail.com>
2025-02-05 01:31:41 +0530

    added projects and experiences

--- lib/data.ts
+++ lib/data.ts
@@ -2,9 +2,10 @@ import React from "react";
 import { CgWorkAlt } from "react-icons/cg";
 import { FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
+import mindsMomImg from "@/public/mindsmom.png";
 import qrollImg from "@/public/qroll.png";
-import rmtdevImg from "@/public/rmtdev.png";
-import wordanalyticsImg from "@/public/wordanalytics.png";
+import portfolioImg from "@/public/portfolio.png"
+// import wordanalyticsImg from "@/public/wordanalytics.png";
 
 export const links = [
   {
@@ -42,25 +43,25 @@ export const experiencesData = [
     title: "Kalinga Institute of Industrial Technology",
     location: "Bhubaneswar, India",
     description:
-      "I graduated after 4 years of studying.",
+      "Currently a third-year undergraduate, I am immersing myself in a diverse and challenging curriculum at KIIT. This experience is enriching my problem-solving skills and fostering creativity, setting a solid foundation for my future in industrial technology.",
     icon: React.createElement(LuGraduationCap),
-    date: "2022 - 2026",
+    date: "September, 2022 - April, 2026",
   },
   {
-    title: "Backend Developer",
-    location: "C",
+    title: "Backend Developer Intern",
+    location: "CXXXXXe,  Bhubaneswar, India",
     description:
-      "N/A",
+      "Worked as a backend developer intern. Due to privacy reasons, I cannot disclose the company name. During my internship, I focused on various backend development tasks, gaining valuable experience in the field.",
     icon: React.createElement(CgWorkAlt),
-    date: "N/A",
+    date: "September, 2024 - November, 2024",
   },
   {
-    title: "N/A",
-    location: "N/A",
+    title: "App Developer",
+    location: "FED Entrepreneurial Society, Bhubaneswar, India",
     description:
-      "N/A",
+      "My contributions include assisting in the development of the front end of a mobile application for iOS/Android using Dart and the Flutter framework. I worked with Google Firebase to manage user-inputted data. Utilized Android Studio as a development environment for both iOS and Android.",
     icon: React.createElement(FaReact),
-    date: "N/A",
+    date: "December, 2023 - Present",
   },
 ] as const;
 
@@ -68,17 +69,31 @@ export const projectsData = [
   {
     title: "MindsMom",
     description:
-      "N/A",
-    tags: ["React", "Next.js", "MongoDB", "Tailwind", "Prisma"],
-    imageUrl: qrollImg,
+      "Made to assess mental health, resources & get guidance. Looking self-help tools or peer support?",
+    tags: ["Kotlin", "OpenAI API", "MongoDB", "Java", "Jetpack Compose"],
+    imageUrl: mindsMomImg,
+    projectUrl:"https://github.com/adityaxanand/MindsMom",
+    githubUrl:"https://github.com/adityaxanand/MindsMom",
   },
 
   {
-    title: "MindsMom",
+    title: "QRoll",
     description:
-      "N/A",
-    tags: ["React", "Next.js", "MongoDB", "Tailwind", "Prisma"],
+      "QR bases attendance system.",
+    tags: ["React", "Node.JS", "MongoDB", "CSS"],
     imageUrl: qrollImg,
+    projectUrl:"https://github.com/adityaxanand/QRoll",
+    githubUrl:"https://github.com/adityaxanand/QRoll",
+  },
+
+  {
+    title: "Personal Portfolio",
+    description:
+      "Used Next.JS to build this personal portfolio website which shows all my achievements and projects.",
+    tags: ["Next.JS", "Express.JS", "MongoDB", "Tailwind", "Framer"],
+    imageUrl: portfolioImg,
+    projectUrl:"https://adityaanand.vercel.app",
+    githubUrl:"https://github.com/adityaxanand/adityaxanand.github.io",
   },
 ] as const;
 
@@ -87,10 +102,13 @@ export const skillsData = [
   "CSS",
   "JavaScript",
   "TypeScript",
+  "Socket.IO",
   "React",
-  "Next.js",
-  "Node.js",
-  "Git",
+  "Next.JS",
+  "Node.JS",
+  "Git/Github",
+  ".NET",
+  "Spring Boot",
   "Tailwind",
   "Prisma",
   "MongoDB",
@@ -102,6 +120,12 @@ export const skillsData = [
   "Python",
   "Django",
   "Framer Motion",
+  "Kubernetes",
+  "Docker",
+  "AWS",
+  "RESTful APIs",
+  "Linux",
+  "Kernel"
 ] as const;
 
 
