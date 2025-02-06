0adcdf97f5500d883c3a6fb8feeb317ed598ec36
Aditya <adityaanandx@gmail.com>
2025-02-06 21:18:32 +0530

    .env

--- pages/api/competitive-programming.ts
+++ pages/api/competitive-programming.ts
@@ -1,3 +1,32 @@
+// // pages/api/competitive-programming.ts
+
+// import type { NextApiRequest, NextApiResponse } from 'next';
+// import { fetchCompetitiveProgrammingData } from '@/lib/cp';
+
+// export default async function handler(
+//   req: NextApiRequest,
+//   res: NextApiResponse
+// ) {
+//   try {
+//     const data = await fetchCompetitiveProgrammingData('adityaxanand');
+
+//     // Log the data for debugging purposes
+//     console.log("Data fetched from Codeforces API:", data);
+
+//     res.status(200).json(data);
+//   } catch (error) {
+//     // Log the error for debugging purposes
+//     console.error("Error fetching data:", error);
+
+//     if (error instanceof Error) {
+//       res.status(500).json({ message: error.message });
+//     } else {
+//       res.status(500).json({ message: 'An unknown error occurred' });
+//     }
+//   }
+// }
+
+
 // pages/api/competitive-programming.ts
 
 import type { NextApiRequest, NextApiResponse } from 'next';
@@ -8,7 +37,14 @@ export default async function handler(
   res: NextApiResponse
 ) {
   try {
-    const data = await fetchCompetitiveProgrammingData('anandxaditya');
+    // Read the username from the environment variable
+    const username = process.env.NEXT_PUBLIC_CODEFORCES_USERNAME;
+
+    if (!username) {
+      throw new Error("Codeforces username not set in environment variables.");
+    }
+
+    const data = await fetchCompetitiveProgrammingData(username);
 
     // Log the data for debugging purposes
     console.log("Data fetched from Codeforces API:", data);
